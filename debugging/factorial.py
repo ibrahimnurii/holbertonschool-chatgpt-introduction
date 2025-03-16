2c37ce5885aef6586db662fb7f221bdce3711abd
Ibrahim Nuri <85269145+ibrahimnurii@users.noreply.github.com>
2025-03-16 22:58:03 +0400

    salam

---
+++ debugging/factorial.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  
+    return result
+
+if len(sys.argv) > 1:
+    try:
+        number = int(sys.argv[1])
+        if number < 0:
+            print("Factorial is not defined for negative numbers.")
+        else:
+            f = factorial(number)
+            print(f)
+    except ValueError:
+        print("Please provide a valid integer.")
+else:
+    print("Usage: python script_name.py <number>")
